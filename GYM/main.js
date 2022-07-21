5454f076bbb619bbab8b4e07b689d3578063ac10
Giank15 <giancarlo_queirolo.15@outlook.es>
2022-07-21 07:32:29 -0500

    Primeras páginas del sitio web y flujo del sitio

--- GYM/main.js
+++ GYM/main.js
@@ -1,23 +1,37 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let html = '';
+html+=`<div>`
+html+=`<center> `
+html+=`<div class="contenedor-formulario contenedor">`
+html+=`<div class="imagen-formulario" > <img src="./images/logo.JPG" alt="" ></div>`
+html+=`<form class="formulario">`
+html+=`<div class="logo"><img src="./images/LOGO NARANJA.png" alt="1200px" width="100px" height="105px"></div>`
+html+=`<div class="texto-formulario">`
+html+=`<h2>Iniciar Sesión</h2>`
+html+=`<p>Ingrese sus credenciales por favor </p>`
+html+=`</div>`
+html+=`<div class="input">`
+html+=`<label for=""></label>`
+html+=`<input placeholder="Usuario" type="text" id="usuario">`
+html+=`</div>`
+html+=`<div class="input contraseña">`
+html+=`<label for=""></label>`
+html+=`<input placeholder="Contraseña" type="password" id="contraseña">`
+html+=`</div>`
+html+=`<div class="input">`
+html+=`<label for=""></label><br>`
+html+=`<a class="ingresar" href="./pages/bienvenido/bienvenido.html">Ingresar</a>`
+html+=`</div>`
+html+=`</form>`
+html+=`</div>`
+html+=`</center>`
+html+=`</div>`
+app.innerHTML = html;
 
-setupCounter(document.querySelector('#counter'))
+/*let boton1 = document.querySelectorAll('.ingresar');
+boton1.forEach(boton => {
+  boton.addEventListener('click', ()=>{
+    window.location.href = "/pages/bienvenido/bienvenido.html";
+  })  
+})*/
---
+++ GYM/pages/bienvenido/bienvenido.js
@@ -0,0 +1,52 @@
+import './estilos.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="inicio">
+    <div class="img1">
+        <img src="../../images/LOGO BLANCO.png" width="60"/>
+     </div>
+    <div class="img2">
+        <img class="mediana" src="../../images/Workout (500 × 130 px).png" width="60"/>
+    </div>
+      <button  class="btn-n0">Cerrar Sesión</button> <br>
+     <div class="img3">
+      <img src="../../images/Logout.png" width="60"/>
+     </div>  
+     </div>
+     <div class="contenedor">
+         <h1>BIENVENIDO, ¿QUE TE GUSTARIA REALIZAR?</h1>
+         <h2>Selecciona una opcion para continuar </h2>
+     </div>
+
+     <div class="contenedor-imagen">
+         <div class="im">
+         <img src="../../images/MenuEmpleado (3).png" alt="imagen no encontrada" width="200px" >
+         <h3>Registrar Cliente Fijo</h3>
+         <button class="btn-n1">REGISTRAR </button>
+         </div>
+         <div class="ime">
+         <img src="../../images/MenuEmpleado (2).png" alt="imagen no encontrada" width="200px">
+         <h3 >Cobro Cliente Eventual</h3>
+         <button class="btn-n2">COBRAR</button> 
+         </div>
+         <div class="imee">
+         <img src="../../images/MenuEmpleado (1).png" alt="imagen no encontrada" width="200px">
+         <h3 >Asignar Horarios Caschs</h3>
+         <button class="btn-n3" k >ASIGNAR</button> 
+         </div>
+     </div>
+  </div>
+`
+let boton1 = document.querySelectorAll('.btn-n0');
+boton1.forEach(boton => {
+  boton.addEventListener('click', ()=>{
+    window.location.href = "../../index.html";
+  })  
+})
+
+let boton2 = document.querySelectorAll('.btn-n1');
+boton2.forEach(boton => {
+  boton.addEventListener('click', ()=>{
+    window.location.href = "../clienteFijo/clienteFijo.html";
+  })  
+})
